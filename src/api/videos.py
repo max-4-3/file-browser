c7bed3fd2cc438269b5195d490f56aa7ad8e8a08
Maximun <rajatvermayt78645@gmail.com>
2025-06-22 01:43:09 +0530

    Added Stats Endpoint

--- src/api/videos.py
+++ src/api/videos.py
@@ -36,3 +36,22 @@ async def get_videos():
             ) for id, d in zip(data.keys(), data.values())
         ]
     )
+
+@router.get('/stats', response_model=VideoPreview)
+async def get_stat(video_id: str):
+    data = get_video_data()
+
+    video_data = data.get(video_id)
+    if not video_data:
+        raise HTTPException(400, detail="Unable to find video's info related to id: {}".format(video_id))
+
+    fp = video_data.get('video_path')
+    if not fp:
+        raise HTTPException(404, detail="File doesn't exist on server!")
+  
+    print(video_data)
+
+    return VideoPreview(
+        id=video_data["id"], title=video_data["title"], modified_time=video_data["m_time"]
+    )
+
