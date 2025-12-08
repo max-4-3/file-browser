856e594b0a4d1ec6f85d002d269ce450536ad264
Maximun <132590174+max-4-3@users.noreply.github.com>
2025-12-08 21:05:08 +0530

    Add PERFORMANCE flag to configuration

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,4 @@ ALLOWED_FILES = {".mp4"}
 ROOT_DIRS = ["/home/max/Videos/.hidden"]
 DATA_FOLDER = r"/home/max/Extras/web/file-browser/data"
 THUMB_PATH = r"/home/max/Extras/web/file-browser/static/.thumbs"
+PERFORMANCE = True
